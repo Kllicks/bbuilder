e20fba961ace497a07040adb5ff5a8d450f4c07a
Kyle Sekellick <sekellickkyle@yahoo.com>
2020-01-07 11:23:16 -0500

    fixed componentwillmount by moving to class initialization

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,14 +6,13 @@ import ContactData from './ContactData/ContactData';
 
 
 class Checkout extends Component {
-    // constructor(props) {
-    //     super(props);
         
-    //     this.state = {
-    //         ingredients: null,
-    //         totalPrice: 0
-    //     }
+    // state = {
+    //     ingredients: null,
+    //     totalPrice: 0
+    // }
 
+    // componentWillMount() {
     //     const query = new URLSearchParams(this.props.location.search);
     //     const ingredients = {};
     //     let price = 0;
@@ -26,28 +25,27 @@ class Checkout extends Component {
     //         }
     //     }
     //     this.setState({ingredients: ingredients, totalPrice: price})
-
     // }
-        
-    state = {
-        ingredients: null,
-        totalPrice: 0
-    }
 
-    componentWillMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0;
-
-        for (let param of query.entries()) {
-            if(param[0] === 'price') {
-                price = param[1];
+    initState = () => {
+        const queryInit = new URLSearchParams(
+            this.props.location.search
+        );
+        const ingredientsInit = {};
+        let priceInit = 0;
+        for (let param of queryInit.entries()) {
+            if (param[0] === 'price') {
+                priceInit = param[1];
             } else {
-                ingredients[param[0]] = +param[1];
+                ingredientsInit[param[0]] = +param[1];
             }
         }
-        this.setState({ingredients: ingredients, totalPrice: price})
-    }
+        return {
+            ingredients: ingredientsInit,
+            price: priceInit
+        };
+    };
+    state = this.initState();
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,13 +5,12 @@ import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-        // constructor(props) {
-        //     super(props);
 
-        //     this.state = {
-        //         error: null
-        //     }
-            
+        // state = {
+        //     error: null
+        // }
+        
+        // componentWillMount() {
         //     this.reqInterceptor = axios.interceptors.request.use(req => {
         //         this.setState({error: null});
         //         return req;
@@ -20,21 +19,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
         //         this.setState({error: error});
         //     });
         // }
-        
 
         state = {
             error: null
-        }
-        
-        componentWillMount() {
-            this.reqInterceptor = axios.interceptors.request.use(req => {
+        };
+
+        reqInterceptor = axios.interceptors.request.use(
+            req => {
                 this.setState({error: null});
                 return req;
-            });
-            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-        }
+            }
+        )
+        
+        resInterceptor = axios.interceptors.response.use(
+            res => res,
+            error => this.setState({error})
+        )
 
         componentWillUnmount() {
             axios.interceptors.request.eject(this.reqInterceptor);
