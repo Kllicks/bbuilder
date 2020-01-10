0307c57d54fec62172c410ce5a0592f995a2cf30
Kyle Sekellick <sekellickkyle@yahoo.com>
2020-01-10 12:34:38 -0500

    add the order reducer

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     
@@ -99,11 +101,8 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-
-        this.setState({loading: true});
         
-        const formData = {};
-        
+        const formData = {};   
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
@@ -113,6 +112,9 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         }
+
+        this.props.onOrderBurger(order);
+
             
     }
 
@@ -212,4 +214,10 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    };
+};
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
