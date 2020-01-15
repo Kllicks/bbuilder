80e2d20edba9d62d6c60fdbcabb212234c5a7cd1
Kyle Sekellick <sekellickkyle@yahoo.com>
2020-01-15 09:57:59 -0500

    adds auth form and sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -20,10 +23,43 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
-    return dispatch => {
-        dispatch(authStart());
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
     }
 };
 
 
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, expirationTime * 1000)
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        const apikey = process.env.REACT_APP_API_KEY;
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apikey}`;
+        if (!isSignup) {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apikey}`;
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                console.log(err.response);
+                dispatch(authFail(err.response.data.error));
+            });
+    };
+};
